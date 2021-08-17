f774f9d2ce28407f02b5bb4cb050449220c79d61
Fel06 <Fel06@users.noreply.github.com>
2021-08-16 22:19:03 -0500

    Se añade la funcion que calcula la nueva mascara.

--- funciones.py
+++ funciones.py
@@ -14,7 +14,7 @@ def convertidor(num_convertir, tipo):
 		if tipo == "d":
 			suma = 0
 			exponente = 0
-			for pos_bit in range(-1,-9,-1):
+			for pos_bit in range(-1,(-1* (octeto.length + 1)),-1):
 				suma += int(octeto[pos_bit]) * (2**exponente)
 				exponente += 1
 				
@@ -42,7 +42,7 @@ def convertidor(num_convertir, tipo):
 				binario = "0"*faltante
 
 			for i in range(-1,(-1 * (contador+1)),-1):
-                                binario += convertido[i]
+				binario += convertido[i]
                         
 			
 
@@ -55,13 +55,31 @@ def convertidor(num_convertir, tipo):
 
 
 ################# HALLAR EXPONENTE ########################
-def exponente(host_solicitado):
+def exponente(hosts_solicitados):
 	for exp in range(0, 9):
 		potenciado = 2**exp
-		diferencia = potenciado -int(host_solicitado)
+		diferencia = potenciado - int(hosts_solicitados)
 		if diferencia >= 2:
 			return exp
 			break
 
 
+################# NUEVA MASCARA ########################
+def nueva_mascara_subnormal(hosts_solicitados, mascara_binaria): # Funcion para calcular la nueva mascara del subnetting normal
+	mascara_vieja_array = mascara_binaria.split(".")
 
+	bits_encender = exponente(hosts_solicitados)
+	cambio = False
+	mascara_nueva = mascara_vieja_array[0]
+	
+	for octeto in mascara_vieja_array:
+		if octeto == "00000000" and cambio == False:
+			encendido = 0
+			while encendido < bits_encender:
+				octeto[encendido] = "1"
+				encendido += 1
+
+			cambio = True
+		mascara_nueva += "." + octeto
+	
+	return mascara_nueva
